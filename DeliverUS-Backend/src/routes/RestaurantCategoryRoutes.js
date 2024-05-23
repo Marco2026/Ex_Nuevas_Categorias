201197adefca21ec7cbed4de05c79d43b92cdbf6
Marco2026 <marherluj@alum.us.es>
2024-05-23 16:16:24 +0200

    post defined in /restaurantCategories

--- DeliverUS-Backend/src/routes/RestaurantCategoryRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantCategoryRoutes.js
@@ -1,7 +1,16 @@
 import RestaurantCategoryController from '../controllers/RestaurantCategoryController.js'
+import * as RestaurantCategoryValidation from '../controllers/validation/RestaurantCategoryValidation.js'
+import { isLoggedIn, hasRole } from '../middlewares/AuthMiddleware.js'
+import { handleValidation } from '../middlewares/ValidationHandlingMiddleware.js'
 
 const loadFileRoutes = function (app) {
   app.route('/restaurantCategories')
     .get(RestaurantCategoryController.index)
+    .post(
+      isLoggedIn,
+      hasRole('owner'),
+      RestaurantCategoryValidation.create,
+      handleValidation,
+      RestaurantCategoryController.create)
 }
 export default loadFileRoutes
